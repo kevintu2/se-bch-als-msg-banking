f48fce0e62a3dec24949cacf5dc01fa13d1e19c4
Rishab Nayak <rishab@bu.edu>
2021-10-31 21:39:39 -0400

    creating register and login endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,61 @@
 # app.py
 
 # Required imports
-import os 
+import os
 from flask import Flask, request, jsonify
 from google.cloud import firestore
+import google.auth.transport.requests
+from google.oauth2 import id_token
+from functools import wraps
+
+HTTP_REQUEST = google.auth.transport.requests.Request()
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Initialize Firestore DB
 db = firestore.Client()
-todo_ref = db.collection('todos')
+users_collection = db.collection('users')
 
-@app.route('/add', methods=['POST'])
-def create():
-    """
-        create() : Add document to Firestore collection with request body.
-        Ensure you pass a custom ID as part of json body in post request,
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
-    """
-    try:
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        auth_header = request.headers['Authorization']
+        if not auth_header:
+            return {'message': 'No token provided'}, 400
+        idtoken = auth_header.split(' ').pop()
+        claims = idtoken.verify_firebase_token(
+            id_token, HTTP_REQUEST, audience=os.environ.get('GOOGLE_CLOUD_PROJECT'))
+        if not claims:
+            return 'Unauthorized', 401
+        return f(*args, **kwargs)
+    return wrap
+
+
+@app.route('/register', methods=['POST'])
+@check_token
+def register():
+    data = request.get_json()
+    if not data:
+        return {'message': 'No data provided'}, 400
+    auth_header = request.headers['Authorization']
+    idtoken = auth_header.split(' ').pop()
+    claims = idtoken.verify_firebase_token(
+        id_token, HTTP_REQUEST, audience=os.environ.get('GOOGLE_CLOUD_PROJECT'))
+    user_ref = users_collection.document(claims['sub'])
+    user_snapshot = user_ref.get()
+    if user_snapshot.exists:
+        return {'message': 'User already exists'}, 400
+    user_ref.set(data)
+    return {'message': 'User created successfully'}, 200
+
+
+@app.route('/login', methods=['GET'])
+@check_token
+def login():
+    return {'message': 'Successfully logged in'}, 200
+
 
 @app.route('/list', methods=['GET'])
 def read():
@@ -45,6 +76,7 @@ def read():
     except Exception as e:
         return f"An Error Occured: {e}"
 
+
 @app.route('/update', methods=['POST', 'PUT'])
 def update():
     """
@@ -59,6 +91,7 @@ def update():
     except Exception as e:
         return f"An Error Occured: {e}"
 
+
 @app.route('/delete', methods=['GET', 'DELETE'])
 def delete():
     """
@@ -71,6 +104,7 @@ def delete():
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
