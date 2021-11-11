199b9d827bf2681bc1c6a5e0a27c5184832fcda1
Rishab Nayak <rishab@bu.edu>
2021-11-11 18:11:16 -0500

    rip

--- backend/app.py
+++ backend/app.py
@@ -51,8 +51,8 @@ def register():
         return {'message': 'No data provided'}, 400
     auth_header = request.headers['Authorization']
     idtoken = auth_header.split(' ').pop()
-    claims = idtoken.verify_firebase_token(
-        id_token, HTTP_REQUEST, audience=os.environ.get('GOOGLE_CLOUD_PROJECT'))
+    claims = id_token.verify_firebase_token(
+        idtoken, HTTP_REQUEST, audience=os.environ.get('GOOGLE_CLOUD_PROJECT'))
     user_ref = users_collection.document(claims['sub'])
     user_snapshot = user_ref.get()
     if user_snapshot.exists:
