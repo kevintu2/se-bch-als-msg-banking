9bf377ffc1c2bf78b509e4c31a7087f3c318b1fa
Jun yue Feng <janefeng@ibm.com>
2021-11-22 23:10:44 -0500

    backend fix

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,8 @@ import google.auth.transport.requests
 from google.oauth2 import id_token
 from functools import wraps
 
+from pyasn1.type.univ import Null
+
 HTTP_REQUEST = google.auth.transport.requests.Request()
 
 # Initialize Flask app
@@ -20,7 +22,7 @@ CORS(app)
 # Initialize Firestore DB
 db = firestore.Client()
 users_collection = db.collection('users')
-user_ref = None
+user = None
 
 # Initialize Cloud Storage
 #CLOUD_STORAGE_BUCKET = os.environ.get('CLOUD_STORAGE_BUCKET')
@@ -133,17 +135,32 @@ def delete():
 
 
 @app.route('/upload_audio', methods=['POST'])
+@check_token
 def upload_audio():
     destination_file_name = f'Audio{uuid.uuid1()}.wav'
+    # print('request.files', request.files)
+    file = request.files['file']
+    fileName  = file.name
+    print(file)
     blob = bucket.blob(destination_file_name)
-    fileName = request.json['fileName']
 
-    blob.upload_from_filename(fileName)
-    data = user_ref.get()
-    if data.audio:
-        user_ref.set(data.audio.append(destination_file_name), merge=True)
+    blob.upload_from_string(file.read(),content_type=file.content_type)
+    auth_header = request.headers['Authorization']
+    idtoken = auth_header.split(' ').pop()
+    claims = id_token.verify_firebase_token(
+        idtoken, HTTP_REQUEST, audience=os.environ.get('GOOGLE_CLOUD_PROJECT'))
+    user_ref = users_collection.document(claims['sub'])
+    doc = user_ref.get()
+    if doc.exists:
+        doc =  doc.to_dict()
+        print(doc)
+        if "audio" in doc:
+            doc["audio"].append({fileName:destination_file_name})
+            user_ref.update( { "audio" : doc["audio"] })
+        else:
+            user_ref.update({ "audio":[{fileName:destination_file_name}]})
     else:
-        user_ref.set([destination_file_name])
+        print(u'No such document!')
     return "File {} uploaded to {}.".format(
         fileName, destination_file_name
     )
@@ -151,13 +168,27 @@ def upload_audio():
 @app.route('/retrieve_audio', methods=['GET'])
 def retrieve_audio():
     fileName = request.json['fileName']
-    destination_file_name = "download.wav"
     blob = bucket.blob(fileName)
-    blob.download_to_filename(destination_file_name)
+
+    url = blob.generate_signed_url(version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=30),
+        # Allow GET requests using this URL.
+        method="GET",)
+    # blob.download_to_filename(destination_file_name)
+    print("Generated GET signed URL:")
+    return url
+
+@app.route('/display_audio', methods=['GET'])
+def display_audio():
+    data = user_ref.get()
+    print("Grabbed User audios")
+    if data.audio:
+        #format is [{userFileName:blobStoreFileName}]
+        return data.audio
+    else:
+        return []
     
-    return "Downloaded storage object {} from bucket {} to local file {}.".format(
-        fileName, CLOUD_STORAGE_BUCKET_NAME, destination_file_name
-    )
 
 
 port = int(os.environ.get('PORT', 8080))
--- client/src/components/pages/Upload.js
+++ client/src/components/pages/Upload.js
@@ -3,14 +3,12 @@ import { auth, logout} from "../firebase";
 import { useHistory } from "react-router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import React, {useEffect} from 'react';
+import axios from "axios";
 
 function Upload() {
     const [user, loading, error] = useAuthState(auth);
-
-    const submitAudio = () => {
-        
-    }
-
+    const [file, setFile] = React.useState();
+    const [title, setTitle] = React.useState();
     const history = useHistory();
     useEffect(() => {
         if (loading) {
@@ -19,17 +17,44 @@ function Upload() {
         }
         if (!user) history.replace("/home");
       }, [user, loading]);
+    const submitAudio = async(event) => {
+        event.preventDefault()
+        let formData = new FormData();
+		formData.append('file', file)
+        formData.append('fileName', title)
+        console.log(file)
+        const token = await auth.currentUser.getIdToken()
+        axios
+        .post("http://localhost:8080/upload_audio", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Authorization": token
+          }
+        })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+          // error response
+          console.log(error)
+        });
+
+    }
+
+    
     return (
         <div>
-            <Form.Group controlId="formFile" class="col-lg-6 offset-lg-3" >
+            <Form enctype="multipart/form-data">
+            <Form.Group controlId="formFile" class="col-lg-6 offset-lg-3"  >
                 <div className="row justify-content-center">
                     <Form.Label>Label your File</Form.Label>
-                    <Form.Control type="input" style={{width:'50%'}} name="title"></Form.Control>
+                    <Form.Control type="input" style={{width:'50%'}} name="title" onChange={(e) => setTitle(e.target.value)}></Form.Control>
                     <Form.Label>Upload your data</Form.Label>
-                    <Form.Control type="file" style={{width:'50%'}} name="fileName"/>
-                    <Form.Control type="submit" onClick={submitAudio} style={{width:'20%', backgroundColor:'#2ca6a4'}}/>
+                    <Form.Control type="file" style={{width:'50%'}} name="file" onChange={(e) => setFile(e.target.files[0])}/>
+                    <Form.Control type="submit"  onClick={submitAudio} style={{width:'20%', backgroundColor:'#2ca6a4'}}/>
                 </div>
             </Form.Group>
+            </Form>
         </div>
     );
 }
