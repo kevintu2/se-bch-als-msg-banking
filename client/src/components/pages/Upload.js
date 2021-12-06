83c221c3ce4b26e6e14db3f5340e788760c7c088
Rishab Nayak <rishab@bu.edu>
2021-12-06 11:29:05 -0500

    upload fix

--- client/src/components/pages/Upload.js
+++ client/src/components/pages/Upload.js
@@ -1,62 +1,74 @@
-import {Form} from 'react-bootstrap';
-import { auth, logout} from "../firebase";
+import { Form } from "react-bootstrap";
+import { auth, logout } from "../firebase";
 import { useHistory } from "react-router";
 import { useAuthState } from "react-firebase-hooks/auth";
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import axios from "axios";
 
 function Upload() {
-    const [user, loading, error] = useAuthState(auth);
-    const [file, setFile] = React.useState();
-    const [title, setTitle] = React.useState();
-    const history = useHistory();
-    useEffect(() => {
-        if (loading) {
-          // maybe trigger a loading screen
-          return;
-        }
-        if (!user) history.replace("/home");
-      }, [user, loading]);
-    const submitAudio = async(event) => {
-        event.preventDefault()
-        let formData = new FormData();
-		formData.append('file', file)
-        formData.append('fileName', title)
-        console.log(file)
-        const token = await auth.currentUser.getIdToken()
-        axios
-          .post("https://api-dev-z2scpwkwva-uc.a.run.app/", formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-              Authorization: token,
-            },
-          })
-          .then((response) => {
-            console.log(response);
-          })
-          .catch((error) => {
-            // error response
-            console.log(error);
-          });
-
+  const [user, loading, error] = useAuthState(auth);
+  const [file, setFile] = React.useState();
+  const [title, setTitle] = React.useState();
+  const history = useHistory();
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
     }
+    if (!user) history.replace("/home");
+  }, [user, loading]);
+  const submitAudio = async (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", title);
+    console.log(file);
+    const token = await auth.currentUser.getIdToken();
+    axios
+      .post("https://api-dev-z2scpwkwva-uc.a.run.app/upload_audio", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        // error response
+        console.log(error);
+      });
+  };
 
-    
-    return (
-        <div>
-            <Form enctype="multipart/form-data">
-            <Form.Group controlId="formFile" class="col-lg-6 offset-lg-3"  >
-                <div className="row justify-content-center">
-                    <Form.Label>Label your File</Form.Label>
-                    <Form.Control type="input" style={{width:'50%'}} name="title" onChange={(e) => setTitle(e.target.value)}></Form.Control>
-                    <Form.Label>Upload your data</Form.Label>
-                    <Form.Control type="file" style={{width:'50%'}} name="file" onChange={(e) => setFile(e.target.files[0])}/>
-                    <Form.Control type="submit"  onClick={submitAudio} style={{width:'20%', backgroundColor:'#2ca6a4'}}/>
-                </div>
-            </Form.Group>
-            </Form>
-        </div>
-    );
+  return (
+    <div>
+      <Form enctype="multipart/form-data">
+        <Form.Group controlId="formFile" class="col-lg-6 offset-lg-3">
+          <div className="row justify-content-center">
+            <Form.Label>Label your File</Form.Label>
+            <Form.Control
+              type="input"
+              style={{ width: "50%" }}
+              name="title"
+              onChange={(e) => setTitle(e.target.value)}
+            ></Form.Control>
+            <Form.Label>Upload your data</Form.Label>
+            <Form.Control
+              type="file"
+              style={{ width: "50%" }}
+              name="file"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <Form.Control
+              type="submit"
+              onClick={submitAudio}
+              style={{ width: "20%", backgroundColor: "#2ca6a4" }}
+            />
+          </div>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
 
 export default Upload;
