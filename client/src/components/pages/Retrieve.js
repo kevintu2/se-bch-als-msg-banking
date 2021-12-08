6792f063ca4fbec9959eb2a127ceb2b129beb9f2
Jun yue Feng <janefeng@ibm.com>
2021-12-08 14:44:27 -0500

    frontend fixes

--- client/src/components/pages/Retrieve.js
+++ client/src/components/pages/Retrieve.js
@@ -4,71 +4,70 @@ import { useHistory } from "react-router";
 import { auth, db, logout } from "../firebase";
 import axios from "axios";
 
-import {Button} from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 function Retrieve() {
-    const [user, loading, error] = useAuthState(auth);
-    const [audio, setAudio] = useState([]);
-    const history = useHistory();
-    const fetchUserAudio = async () => {
-        try {
-          const query = await db
-            .collection("users")
-            .where("uid", "==", user?.uid)
-            .get();
-          const data = await query.docs[0].data();
-          if ("audio" in data){
-            // console.log(typeoaudio.concat(data.audio));
-            setAudio( audio.concat(data.audio));
-          }
-          else{
-            setAudio([])
-          }
-        } catch (err) {
-          console.error(err);
-          alert("An error occured while fetching user data");
-        }
-      };
-      useEffect(() => {
-        if (loading) return;
-        if (!user) return history.replace(process.env.PUBLIC_URL +"/");
-        fetchUserAudio();
-      },[loading]);
-      const downloadClip = async (name) => {
-        try {
-          const response = await axios.post(
-            "https://api-dev-z2scpwkwva-uc.a.run.app/retrieve_audio",
-            {
-              fileName: name,
-            }
-          );
-          window.open(response.data);
-        } catch (err) {
-          console.log(err)
-        }
+  const [user, loading, error] = useAuthState(auth);
+  const [audio, setAudio] = useState([]);
+  const history = useHistory();
+  const fetchUserAudio = async () => {
+    try {
+      const query = await db
+        .collection("users")
+        .where("uid", "==", user?.uid)
+        .get();
+      const data = await query.docs[0].data();
+      if ("audio" in data) {
+        setAudio(audio.concat(data.audio));
+      } else {
+        setAudio([]);
       }
-      var audioList = audio.map( value => {
-            const res = Object.entries(value);
-            console.log(res)
-            return (
-              <button key="{res}" onClick={() => downloadClip(res[0][1])}>
-                {res[0][0]}
-              </button>
-            );
-          })
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return history.replace(process.env.PUBLIC_URL + "/");
+    fetchUserAudio();
+  }, [loading]);
+  const downloadClip = async (name) => {
+    try {
+      const response = await axios.post(
+        "https://api-dev-z2scpwkwva-uc.a.run.app/retrieve_audio",
+        {
+          fileName: name,
+        }
+      );
+      window.open(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  var audioList = audio.map((value) => {
+    const res = Object.entries(value);
+    console.log(res);
     return (
-        <>
-        <h1 className="dashboard-header text-center">
-            Rediscover your voice
-        </h1>
-        <h2 className="dashboard-header text-center">
-            Click to download your voice!
-        </h2>
-        <br/>
-       {audioList}
-        </>
-    )
-
+      <li>
+        <button className="align-center mt-2" key="{res}" onClick={() => downloadClip(res[0][1])}>
+          {res[0][0]}
+        </button>
+      </li>
+    );
+  });
+  return (
+    <>
+      <h1 className="dashboard-header text-center">Rediscover your voice</h1>
+      <h2 className="dashboard-header text-center">
+        Click to download your voice!
+      </h2>
+      <br />
+      <div class="col-md-12 text-center">
+      <ul>{audioList}</ul>
+      </div>
+    </>
+  );
 }
 
 export default Retrieve;
